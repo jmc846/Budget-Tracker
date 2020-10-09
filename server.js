ec04a95f3b2d3abb842c7e5137258a91563c08e5
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-10-09 15:34:11 -0400

    changes to htmlRoutes

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ mongoose.connect(
 ;
 
 // routes
-app.use(require("./routes/api.js"));
-
+app.use(require("./routes/api"));
+app.use(require("./routes/htmlRoutes"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
