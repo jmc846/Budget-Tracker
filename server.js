930892982abb79fa8ccf65f7cb74cdb1f897ad1f
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-10-09 15:48:23 -0400

    fices

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ mongoose.connect(
 
 // routes
 app.use(require("./routes/api"));
-if (process.env.NODE_ENV === "production"{
+if (process.env.NODE_ENV === "production"){
   app.use(express.static("build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname,  "build", "index.html"));
