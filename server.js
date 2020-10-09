3227d78061bc4c247346d0e4739e46bf9958067c
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-10-09 15:20:27 -0400

    added files for Heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 8181;
+const PORT = 8181 || process.env.PORT
 
 const app = express();
 
